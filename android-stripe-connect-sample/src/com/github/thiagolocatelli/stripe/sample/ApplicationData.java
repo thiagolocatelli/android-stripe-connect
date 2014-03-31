254ee05dde2ef52d49c97ce916edb122eed40dcb
Thiago Locatelli <thiago.locatelli@gmail.com>
2014-03-31 19:31:03 -0400

    Initial Commit

--- android-stripe-connect-sample/src/com/github/thiagolocatelli/stripe/sample/ApplicationData.java
+++ android-stripe-connect-sample/src/com/github/thiagolocatelli/stripe/sample/ApplicationData.java
@@ -1,7 +1,7 @@
 package com.github.thiagolocatelli.stripe.sample;
 
 public class ApplicationData {
-	public static final String CLIENT_ID = "ca_3kxZDfgu1egCUZ0Duasq0TxWFch1M3eu";
-	public static final String SECRET_KEY = "Sryu616aCXjQS80AdYZrmsiDx1c7iXu9";
-	public static final String CALLBACK_URL = "https://android.stripe.connect.demo";
+	public static final String CLIENT_ID = "";
+	public static final String SECRET_KEY = "";
+	public static final String CALLBACK_URL = "";
 }
